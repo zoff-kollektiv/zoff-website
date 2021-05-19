989bf74af91173dcd0c1448c88cf89885655a8ed
Andreas Neumann <andneuma@posteo.de>
2021-05-19 12:33:00 +0200

    Fix warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,79 +10,82 @@ const IndexPage = ({ data }) => {
   const [remainingProjects, setRemainingProjects] = useState([])
   const [displayedProjects, setDisplayedProjects] = useState([])
 
-  useEffect(() => {
-    initSelectionSchema()
-  }, [])
-  useEffect(() => {
-    initRemainingProjects()
-  }, [selectionSchema])
-
+  // Generate random integer between min and (max-1)
   const getRndInteger = (min, max) => {
     return Math.floor(Math.random() * (max - min)) + min
   }
 
-  const initSelectionSchema = () => {
-    let schema = data.allMarkdownRemark.edges
-      .map(category => [
-        category.node.frontmatter.order,
-        category.node.frontmatter.title,
-      ])
-      .sort((a, b) => (a[0] > b[0] ? 1 : -1))
-      .map(category => category[1])
-    setSelectionSchema(schema)
-  }
-
-  const initRemainingProjects = () => {
-    if (selectionSchema.length > 0) {
-      // Split up projects assigned to respective category
-      let projects = {}
-      let remProjects = []
-
-      data.allMarkdownRemark.edges.forEach(category => {
-        const categoryTitle = category.node.frontmatter.title
-        projects[categoryTitle] = category.node.frontmatter.projects
-      })
-
-      // Maximum number of images within the categories
-      const maxImageNo = Math.max(
-        ...Object.values(projects).map(val => val.length)
-      )
-
-      // Array containing regular sequence of indices
-      // Gonna be [b, c, a, b, c, a, ...]
-      let selectionIndexArray = Array(maxImageNo).fill(selectionSchema).flat()
-      const startingPoint = getRndInteger(0, 4)
-      const firstElementsToBack = selectionIndexArray.slice(0, startingPoint)
-      selectionIndexArray = selectionIndexArray.slice(
-        startingPoint,
-        selectionIndexArray.length
-      )
-      selectionIndexArray.push(...firstElementsToBack);
-
-      // Traverse projects array following selectionSchema and startingPoint
-      // and fill array of images / projects remaining with random image from category
-      selectionIndexArray.forEach(index => {
-        let randomIndex = getRndInteger(0, projects[index].length)
-        let newItem = projects[index][randomIndex]
-        remProjects.push(newItem)
-
-        // Remove item from Array
-        projects[index] = projects[index]
-          .slice(0, randomIndex)
-          .concat(
-            projects[index].slice(randomIndex + 1, projects[index].length)
-          )
-      })
-
-      // Reverse array of remaining projects and filter out undefined
-      remProjects = remProjects.reverse().filter(Boolean)
-
-      // Display first image initially as soon as something is there (async!)
-      if (remProjects.length > 0) setDisplayedProjects([remProjects.pop()])
-
-      setRemainingProjects(remProjects)
+  // Fetch order from backend data and generate project category selection schema
+  useEffect(() => {
+    const initSelectionSchema = () => {
+      let schema = data.allMarkdownRemark.edges
+        .map(category => [
+          category.node.frontmatter.order,
+          category.node.frontmatter.title,
+        ])
+        .sort((a, b) => (a[0] > b[0] ? 1 : -1))
+        .map(category => category[1])
+      setSelectionSchema(schema)
     }
-  }
+
+    initSelectionSchema()
+  }, [data])
+
+  // Fill remaining projects array on page load
+  useEffect(() => {
+    const initRemainingProjects = () => {
+      if (selectionSchema.length > 0) {
+        // Split up projects assigned to respective category
+        let projects = {}
+        let remProjects = []
+
+        data.allMarkdownRemark.edges.forEach(category => {
+          const categoryTitle = category.node.frontmatter.title
+          projects[categoryTitle] = category.node.frontmatter.projects
+        })
+
+        // Maximum number of images within the categories
+        const maxImageNo = Math.max(
+          ...Object.values(projects).map(val => val.length)
+        )
+
+        // Array containing regular sequence of indices
+        // Gonna be [b, c, a, b, c, a, ...]
+        let selectionIndexArray = Array(maxImageNo).fill(selectionSchema).flat()
+        const startingPoint = getRndInteger(0, 4)
+        const firstElementsToBack = selectionIndexArray.slice(0, startingPoint)
+        selectionIndexArray = selectionIndexArray.slice(
+          startingPoint,
+          selectionIndexArray.length
+        )
+        selectionIndexArray.push(...firstElementsToBack)
+
+        // Traverse projects array following selectionSchema and startingPoint
+        // and fill array of images / projects remaining with random image from category
+        selectionIndexArray.forEach(index => {
+          let randomIndex = getRndInteger(0, projects[index].length)
+          let newItem = projects[index][randomIndex]
+          remProjects.push(newItem)
+
+          // Remove item from Array
+          projects[index] = projects[index]
+            .slice(0, randomIndex)
+            .concat(
+              projects[index].slice(randomIndex + 1, projects[index].length)
+            )
+        })
+
+        // Reverse array of remaining projects and filter out undefined
+        remProjects = remProjects.reverse().filter(Boolean)
+
+        // Display first image initially as soon as something is there (async!)
+        if (remProjects.length > 0) setDisplayedProjects([remProjects.pop()])
+
+        setRemainingProjects(remProjects)
+      }
+    }
+    initRemainingProjects()
+  }, [data, selectionSchema])
 
   const displayNextProject = () => {
     if (remainingProjects.length > 0) {
@@ -97,18 +100,20 @@ const IndexPage = ({ data }) => {
   const Projects = () => {
     return displayedProjects
       .map((project, index) => {
-        const isFirst = index == displayedProjects.length - 1;
-        const notLast = remainingProjects.length != 0;
+        const isFirst = index === displayedProjects.length - 1
+        const notLast = remainingProjects.length !== 0
 
         return (
           // Image container
           <div
-            key={project.title}
+            key={`project-${index}`}
             className="image-container"
+            role="button"
+            tabIndex="0"
             onClick={isFirst ? displayNextProject : undefined}
+            onKeyPress={undefined}
             style={{ width: `${project.scale}%` }}
           >
-
             {/* Image */}
             <Image
               className={isFirst && notLast ? "first-image" : ""}
@@ -116,7 +121,16 @@ const IndexPage = ({ data }) => {
             />
 
             {/* + Sign */}
-            {isFirst && notLast ? <div className="image-cross-overlay" style={{fontSize: `${project.scale/2}vw`}}>＋</div> : ""}
+            {isFirst && notLast ? (
+              <div
+                className="image-cross-overlay"
+                style={{ fontSize: `${project.scale / 2}vw` }}
+              >
+                ＋
+              </div>
+            ) : (
+              ""
+            )}
             <figcaption>{project.caption}</figcaption>
           </div>
         )
@@ -153,7 +167,7 @@ export const query = graphql`
               project_url
               scale
             }
-            
+
             title
             order
           }
